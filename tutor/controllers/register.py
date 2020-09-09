bde15be008cd3f02726baddf9f9889fd406e435e
golan13 <55846420+golan13@users.noreply.github.com>
2020-09-09 17:06:49 +0300

    Update register.py

--- tutor/controllers/register.py
+++ tutor/controllers/register.py
@@ -4,5 +4,5 @@ from .forms import RegistrationForm, LoginForm
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        return redirect(url_for('get_course_info_route', id=1))
+        return redirect(url_for('index_route'))
     return render_template('register.html', form=form)

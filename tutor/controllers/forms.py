881894a2eb248d53985452b690f817fc33f17533
Golan Cohen <golan13@gmail.com>
2020-09-12 13:50:12 +0300

    Added validators for email and username if exist in DB

--- tutor/controllers/forms.py
+++ tutor/controllers/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField
-from wtforms.validators import DataRequired, Length, EqualTo, InputRequired
+from wtforms.validators import DataRequired, Length, EqualTo, InputRequired, ValidationError
+from ..models.users import Users
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -12,6 +14,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                 validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first
+        if user:
+            raise ValidationError('Email already registered. Login or choose a different email.')
+
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first
+        if user:
+            raise ValidationError('Username already registered. Please choose a different one.')
+
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',

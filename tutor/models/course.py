3cea5008a544c5a2e3d24f21385adc9e9d7a58c4
Lior <lior_reytan@milab.idc.ac.il>
2020-09-14 22:16:05 +0300

    Added init of course model

--- tutor/models/course.py
+++ tutor/models/course.py
@@ -5,3 +5,6 @@ class Course(db.Model):
     __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, natitleme):
+        self.title = title

287ec2ae226d1bda2e7d3d647e11f181f02b0c1f
Lior <lior_reytan@milab.idc.ac.il>
2020-09-14 16:45:20 +0300

    Fixed course model

--- tutor/models/course.py
+++ tutor/models/course.py
@@ -4,4 +4,3 @@ class Course(db.Model):
     __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    resources = db.relationship('Resource', backref='course', lazy=True)

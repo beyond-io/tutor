f596d8de57b4fdcb32a961601202655713e692b4
Lior <lior_reytan@milab.idc.ac.il>
2020-09-16 18:28:52 +0300

    Fixed again course model

--- tutor/models/course.py
+++ tutor/models/course.py
@@ -6,5 +6,8 @@ class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, natitleme):
+    def __init__(self, title):
         self.title = title
+
+    def __repr__(self):
+        return f"Course('{self.title}')"

d66301c3a9d726be00d4d12f70f9ec4a02a5ada3
Michal-Shamir <michalsham4@gmail.com>
2020-10-19 15:22:53 +0300

    lint fix

--- tutor/controllers/resources.py
+++ tutor/controllers/resources.py
@@ -14,18 +14,19 @@ def newResource(id):
         return redirect(url_for('get_course_info_route', id=id))
     return render_template('resource.html', form=form)
 
+
 def updateResource(id, resource_id):
-    resource=Resource.query.filter_by(id=resource_id).first()
+    resource = Resource.query.filter_by(id=resource_id).first()
     form = ResourceForm()
     if form.validate_on_submit():
-        resource.title=form.title.data
-        resource.content=form.content.data
-        resource.link=form.link.data
+        resource.title = form.title.data
+        resource.content = form.content.data
+        resource.link = form.link.data
         db.session.commit()
         return redirect(url_for('get_course_info_route', id=id))
     elif request.method == 'GET':
-        form.title.data=resource.title
-        form.content.data=resource.content
-        form.link.data=resource.link
+        form.title.data = resource.title
+        form.content.data = resource.content
+        form.link.data = resource.link
 
     return render_template('resource.html', form=form)
